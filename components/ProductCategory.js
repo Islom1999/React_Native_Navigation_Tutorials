c92ad6a74630be101ab34e975b21b7659cbe44b8
islomkarimovalisherovich <islomkarimovalisherovich@gmail.com>
2023-01-28 17:58:54 +0500

    React Native Navigation, HomeScreen, Navigation

---
+++ components/ProductCategory.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function ProductCategoryScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Product Category Screen</Text>
+    </View>
+  );
+}
+
+export default ProductCategoryScreen;
